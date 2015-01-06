aadc637504090366587564a98e5fd37a78b5fda9
Fajkowsky <dawidfajkowski@gmail.com>
2015-01-06 16:17:28 +0100

    initial commit, added setup.py and gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pyKindle',
+      version='0.0.1',
+      description='Tool for generating mobi files for Kindle ebook readers.',
+      author='Dawid Fajkowski',
+      author_email='dawid.fajkowski@gmail.com',
+      install_requires=[
+          'jinja2 >= 2.7.3'
+      ]
+)
