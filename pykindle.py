f371aac702f905761ab3bc7614c541b1da2484a0
Fajkowsky <dawidfajkowski@gmail.com>
2015-01-09 13:28:36 +0100

    added argparse and first argument(directory path), logging also added

--- pykindle.py
+++ pykindle.py
@@ -1,5 +1,29 @@
+import argparse
+import os
+import logging
+
+
+def create_parser():
+    help = {
+        'directory': 'Use directory where files are stored.'
+    }
+    parser = argparse.ArgumentParser(description='Create mobi file.')
+    parser.add_argument('directory', help=help['directory'])
+    return parser
+
+
+def check_dir(path):
+    return os.path.isdir(path)
+
+
 def main():
-    pass
+    parser = create_parser()
+    args = parser.parse_args()
+    if check_dir(args.directory):
+        logging.info('Directory found.')
+    else:
+        logging.warning('Directory not exists.')
+
 
 if __name__ == '__main__':
     main()
