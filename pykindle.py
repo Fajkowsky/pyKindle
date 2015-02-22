1a2833d074fe4df2f04625aa6a0ae0d60bbda497
Fajkowsky <dawidfajkowski@gmail.com>
2015-02-22 11:22:56 +0100

    added few methods for handling dirs and split started splitting code

--- pykindle.py
+++ pykindle.py
@@ -1,30 +1,55 @@
 import argparse
 import os
 import logging
+import re
+
+# SETTINGS
+BUILD_DIR = 'build/'
 
 
-class BookCreator:
-    def __init__(self, directory):
-        self.directory = directory
-
-    def create(self):
-        pass
-
-    def build(self):
-        pass
+# UTILS
+def create_dir(dir=BUILD_DIR):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
 
-def create_parser():
-    help = {
-        'directory': 'Use directory where files are stored.'
-    }
-    parser = argparse.ArgumentParser(description='Create mobi file.')
-    parser.add_argument('directory', help=help['directory'])
-    return parser
+def delete_dir(dir=BUILD_DIR):
+    if os.path.exists(dir):
+        os.rmdir(dir)
 
 
 def check_dir(path):
     return os.path.isdir(path)
+
+
+# MAIN PACKAGE
+class BookCreator:
+    def __init__(self, directory):
+        self.directory = directory
+        self.files = self.search_files()
+
+    def search_files(self):
+        return sorted([
+            file for file in os.listdir(self.directory)
+            if re.match(r'\d+.txt', file)
+        ])
+
+    def create(self):
+        create_dir()
+
+
+    def build(self):
+        delete_dir()
+
+
+# RUN
+def create_parser():
+    helpers = {
+        'directory': 'Use directory where files are stored.'
+    }
+    parser = argparse.ArgumentParser(description='Create mobi file.')
+    parser.add_argument('directory', help=helpers['directory'])
+    return parser
 
 
 def main():
@@ -32,6 +57,7 @@ def main():
     args = parser.parse_args()
     if check_dir(args.directory):
         logging.info('Directory found.')
+
         bc = BookCreator(args.directory)
         bc.create()
         bc.build()
